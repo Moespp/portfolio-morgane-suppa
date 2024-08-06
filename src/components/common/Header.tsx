740ef9ae3a8158e4d08887f43f4fcf59767d33d9
amlds <45981954+amlds@users.noreply.github.com>
2024-08-06 15:24:30 +0200

    update header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
@@ -12,12 +14,12 @@ const Header = () => {
         >
           Acceuil
         </a>
-        <a
-          href="#"
-          className="text-sm font-semibold leading-6 text-black hover:text-primary transition-all duration-300"
-        >
-          Contact
-        </a>
+        <Button
+          onClick={() => {
+            window.open("https://discord.gg/3w7k2u8", "_blank");
+          }}
+          label="Contact"
+        />
       </div>
     </header>
   );
