80ac3aeec57d142c028910326e35b70ce1da474b
amlds <45981954+amlds@users.noreply.github.com>
2024-08-06 15:28:51 +0200

    update css padding

--- src/pages/home/section/ListProjects.tsx
+++ src/pages/home/section/ListProjects.tsx
@@ -4,7 +4,7 @@ import CardProject from "../../../components/card/CardProject";
 
 const ListProjects = () => {
   return (
-    <div className="flex flex-col items-center gap-4 w-full max-w-[1500px] mx-auto sm:px-8 px-4">
+    <div className="flex flex-col items-center gap-24 w-full max-w-[1500px] mx-auto sm:px-8 px-4 sm:py-16 py-8">
       <Title text="Mes projets" />
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
         {projects.map((project) => (
