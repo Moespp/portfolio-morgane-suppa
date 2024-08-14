da3116f5eb41ed8568a6b4afa5fffe1fed98ba5f
amlds <45981954+amlds@users.noreply.github.com>
2024-08-14 17:16:25 +0200

    update je sais plus

--- src/data/projects/DataProjects.ts
+++ src/data/projects/DataProjects.ts
@@ -38,6 +38,43 @@ const projects: ProjectDTO[] = [
       gameplayImageCard4: "../img/project/noreya/IMG_9372.jpg",
     },
   },
+  {
+    id: "2",
+    title: "Red did not die",
+    img: "./img/project/noreya/IMG_9372.jpg",
+    description:
+      "Noreya is a visual novel written by Morgane and illustrated by herself. It is a dark fantasy adventure set in a world where magic is forbidden and the only way to survive is to find and use the power of the crows.",
+    type: "Pro",
+    tags: ["Level Designer", "Game Design", "Narrative Designer", "Scripting"],
+    link: "https://www.a-crows-nest.com/",
+    page: {
+      backgroundImage: "../img/project/noreya/IMG_9372.jpg",
+      moteur: "DreamEngine",
+      tools: "Tiled, Miro, Trello, Discord",
+      plateforme: ["Steam", "Switch", "Xbox", "Playstation"],
+      gameType: ["metroidvania", "pixelart", "2D", "fantasy"],
+      video: "https://www.youtube.com/watch?v=0-1-2-3",
+      pathi18n: "project",
+      conceptionImage: [
+        "../img/project/noreya/IMG_9372.jpg",
+        "../img/project/noreya/IMG_9372.jpg",
+      ],
+      blockoutImage: [
+        "../img/project/noreya/IMG_9372.jpg",
+        "../img/project/noreya/IMG_9372.jpg",
+      ],
+      renduImage: [
+        "../img/project/noreya/IMG_9372.jpg",
+        "../img/project/noreya/IMG_9372.jpg",
+      ],
+      gameplayImageCard1: "../img/project/noreya/IMG_9372.jpg",
+
+      gameplayImageCard2: "../img/project/noreya/IMG_9372.jpg",
+
+      gameplayImageCard3: "../img/project/noreya/IMG_9372.jpg",
+      gameplayImageCard4: "../img/project/noreya/IMG_9372.jpg",
+    },
+  },
 ];
 
 export default projects;
