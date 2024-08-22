c8a8e63ce7d939926a67bd884ba711d3aff0f0aa
amlds <45981954+amlds@users.noreply.github.com>
2024-08-23 00:48:53 +0200

    little update texte reaveal translation

--- src/components/common/TextReveal.tsx
+++ src/components/common/TextReveal.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
+import { useTranslation } from "react-i18next";
 
 interface TextRevealProps {
   words: string[];
@@ -7,6 +8,7 @@ interface TextRevealProps {
 }
 
 const TextReveal: React.FC<TextRevealProps> = ({ words, duration }) => {
+  const { t } = useTranslation();
   const textRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -20,52 +22,69 @@ const TextReveal: React.FC<TextRevealProps> = ({ words, duration }) => {
     words.forEach((word) => {
       let localIndexWord = 0;
 
-      tl.call(() => {
-        if (textRef.current) {
-          textRef.current.innerHTML = "";
-        }
-      }, [], "+=0");
+      tl.call(
+        () => {
+          if (textRef.current) {
+            textRef.current.innerHTML = "";
+          }
+        },
+        [],
+        "+=0"
+      );
 
       word.split("").forEach((letter) => {
-        tl.call(() => {
-          if (textRef.current) {
-            const span = document.createElement("span");
-            span.textContent = letter;
-            span.style.display = "inline-block";
-            span.style.opacity = "0";
-            textRef.current.appendChild(span);
+        tl.call(
+          () => {
+            if (textRef.current) {
+              const span = document.createElement("span");
+              span.textContent = letter;
+              span.style.display = "inline-block";
+              span.style.opacity = "0";
+              textRef.current.appendChild(span);
 
-            gsap.to(span, {
-              opacity: 1,
-              duration: 0.2,
-            });
+              gsap.to(span, {
+                opacity: 1,
+                duration: 0.2,
+              });
 
-            localIndexWord += 1;
-          }
-        }, [], "+=0.1");
+              localIndexWord += 1;
+            }
+          },
+          [],
+          "+=0.1"
+        );
       });
 
       tl.to(textRef.current, {
         duration: duration,
       });
 
-      word.split("").reverse().forEach(() => {
-        tl.call(() => {
-          if (textRef.current && textRef.current.children.length > 0) {
-            const span = textRef.current.children[textRef.current.children.length - 1] as HTMLElement;
+      word
+        .split("")
+        .reverse()
+        .forEach(() => {
+          tl.call(
+            () => {
+              if (textRef.current && textRef.current.children.length > 0) {
+                const span = textRef.current.children[
+                  textRef.current.children.length - 1
+                ] as HTMLElement;
 
-            gsap.to(span, {
-              opacity: 0,
-              duration: 0.05,
-              onComplete: () => {
-                if (span.parentNode) {
-                  span.parentNode.removeChild(span);
-                }
-              },
-            });
-          }
-        }, [], "+=0.1");
-      });
+                gsap.to(span, {
+                  opacity: 0,
+                  duration: 0.05,
+                  onComplete: () => {
+                    if (span.parentNode) {
+                      span.parentNode.removeChild(span);
+                    }
+                  },
+                });
+              }
+            },
+            [],
+            "+=0.1"
+          );
+        });
     });
 
     return () => {
@@ -82,7 +101,7 @@ const TextReveal: React.FC<TextRevealProps> = ({ words, duration }) => {
         whiteSpace: "nowrap",
       }}
     >
-      Je suis{" "}
+      {t("common.iam")}{" "}
       <div
         style={{
           display: "inline-block",
@@ -90,7 +109,10 @@ const TextReveal: React.FC<TextRevealProps> = ({ words, duration }) => {
           overflow: "hidden",
         }}
       >
-        <div ref={textRef} style={{ display: "inline-block", width: 'fit-content' }} />
+        <div
+          ref={textRef}
+          style={{ display: "inline-block", width: "fit-content" }}
+        />
       </div>
     </div>
   );
